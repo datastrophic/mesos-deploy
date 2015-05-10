c0b0771a785043dbc526c2de8bd95e8ee0952860
Anton Kirillov <akirillov.mailbox@gmail.com>
2015-05-10 14:43:26 +0200

    hardy-specific setup

--- mesos-ansible-ha/provision.sh
+++ mesos-ansible-ha/provision.sh
@@ -4,4 +4,4 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 #to run this against brand new cluster 'ansible' user should be created with passwordless sudo privileges
 #and your public key (this can be automated with 'user-data' on many cloud providers)
-ansible-playbook playbook.yml -i hosts -c ssh -u ansible --private-key=~/.ssh/id_rsa
+ansible-playbook playbook.yml -i hosts -c ssh -u akirillov --private-key=~/.ssh/id_rsa
